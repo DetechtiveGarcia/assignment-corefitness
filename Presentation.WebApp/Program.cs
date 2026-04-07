5018c6aae29575ce490fb5d9974a075aa204bf7a
migue <migue@LAPTOP-LIQQFHEB>
2026-04-07 13:26:31 +0200

    feat(navigation): add dynamic menu service and route token transformer

--- Presentation.WebApp/Program.cs
+++ Presentation.WebApp/Program.cs
@@ -1,10 +1,19 @@
 using Application;
 using Infrastructure;
+using Microsoft.AspNetCore.Mvc.ApplicationModels;
+using Presentation.WebApp.Routing;
+using Presentation.WebApp.Services.MenuNavigation;
 
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddControllersWithViews(options =>
+{
+    options.Conventions.Add(new RouteTokenTransformerConvention(new MenuUrlRewriter()));
+});
+
 builder.Services.AddApplication(builder.Configuration, builder.Environment);
 builder.Services.AddInfrastrcuture(builder.Configuration, builder.Environment);
+builder.Services.AddScoped<IMenuNavigationService, MenuNavigationService>();
 
 builder.Services.AddSession();
 builder.Services.AddRouting(x => x.LowercaseUrls = true);
