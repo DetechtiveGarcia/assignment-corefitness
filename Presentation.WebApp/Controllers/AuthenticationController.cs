a9057f1d36cdf524ecf64434caef8dbe220bc20b
migue <migue@LAPTOP-LIQQFHEB>
2026-04-08 17:33:15 +0200

    feat(authentication): add signup routes and link routes header to auth flow

---
+++ Presentation.WebApp/Controllers/AuthenticationController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Presentation.WebApp.Attributes.MenuNavigation;
+
+namespace Presentation.WebApp.Controllers;
+
+public class AuthenticationController : Controller
+{
+    [HideInMenu]
+    public IActionResult SignUp()
+    {
+        return View();
+    }
+
+    [HideInMenu]
+    public IActionResult SetPassword()
+    {
+        return View();
+    }
+}
