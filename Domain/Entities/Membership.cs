2a73ebe489303e1071faef12d9d1c3845cc10732
DetectiveGarcia <144781010+DetectiveGarcia@users.noreply.github.com>
2026-04-12 18:18:37 +0200

    feat(domain): add member and membership entities

---
+++ Domain/Entities/Membership.cs
@@ -0,0 +1,9 @@
+﻿namespace Domain.Entities;
+public class Membership
+{
+    public string Id { get; private set; } = null!;
+    public string? MemberId { get; private set; }
+    public string? MembershipOptionId { get; private set; }
+    public DateTimeOffset? StartDate { get; private set; } //DateTime eller DateTimeOffset??
+    public DateTime? EndDate { get; private set; }
+}
